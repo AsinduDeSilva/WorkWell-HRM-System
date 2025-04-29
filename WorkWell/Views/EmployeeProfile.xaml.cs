bd6f0a1c4fe16bbef82fbcf76e362c74beb3cafc
DewmiS <dewmisamadhipathiranage@gmail.com>
2025-04-29 15:13:37 +0530

    Created Profile View

---
+++ WorkWell/Views/EmployeeProfile.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+
+namespace WorkWell.Views
+{
+    
+    public partial class EmployeeProfile : Page
+    {
+        public EmployeeProfile()
+        {
+            InitializeComponent();
+        }
+    }
+}
