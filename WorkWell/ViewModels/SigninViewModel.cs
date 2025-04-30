35c77e0298b9452c2717cc46311e19b80c4cb510
Asindu De Silva <asindudesilva@gmail.com>
2025-04-30 19:39:01 +0530

    Implemented Sign In

---
+++ WorkWell/ViewModels/SigninViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System.Windows;
+using WorkWell.Data;
+using WorkWell.Enums;
+using WorkWell.MVVM;
+using WorkWell.Services;
+using WorkWell.Views.Admin;
+
+namespace WorkWell.ViewModels
+{
+    class SigninViewModel : ViewModelBase
+    {
+        private string email;
+
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                email = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string password;
+
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                password = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public RelayCommand LoginCommand => new RelayCommand(execute => Login());
+
+        private AppDbContext context;
+
+        public SigninViewModel()
+        {
+            context = new AppDbContext();
+        }
+        public void Login()
+        {
+            foreach (var user in context.Users.ToList())
+            {
+                if (user.Email == email && user.Password == password)
+                {
+                    if (user.Role.Equals(RoleTypes.ADMIN.ToString()))
+                    {
+                        FrameManagerService.MainFrame.Navigate(new AdminView());
+                    }
+                    else if (user.Role.Equals(RoleTypes.HRM.ToString()))
+                    {
+
+
+                    }
+                    else if (user.Role.Equals(RoleTypes.EMPLOYEE.ToString()))
+                    {
+                       
+                    }
+                    return;
+                }
+            }
+            MessageBox.Show("Invalid email or password", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+}
+
--- WorkWell/Views/Signin.xaml.cs
+++ WorkWell/Views/Signin.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using WorkWell.ViewModels;
 
 namespace WorkWell.Views
 {
@@ -23,11 +12,16 @@ namespace WorkWell.Views
         public Signin()
         {
             InitializeComponent();
+            DataContext = new SigninViewModel();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
         {
-            
+            if (this.DataContext is SigninViewModel viewModel)
+            {
+                viewModel.Password = PasswordBox.Password;
+            }
         }
     }
 }
