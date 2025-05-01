58897cae84cf18aa4c698a0a6623e7b9b7b70c44
DewmiS <dewmisamadhipathiranage@gmail.com>
2025-05-01 22:12:53 +0530

    Updated SettingsPage

--- WorkWell/Views/Employee/Settings.xaml.cs
+++ WorkWell/Views/Employee/Settings.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using WorkWell.ViewModels;
 
 namespace WorkWell.Views.Employee
 {
@@ -7,11 +9,43 @@ namespace WorkWell.Views.Employee
         public Settings()
         {
             InitializeComponent();
+            this.DataContext = new SettingsViewModel();
         }
 
-        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
         {
+            txtCurrentPassword.Password = string.Empty;
+            txtNewPassword.Password = string.Empty;
+            txtConfirmPassword.Password = string.Empty;
 
+            if (DataContext is SettingsViewModel viewModel)
+            {
+                viewModel.Cancel();
+            }
+        }
+
+        private void txtCurrentPassword_PasswordChanged(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (this.DataContext is SettingsViewModel viewModel)
+            {
+                viewModel.CurrentPassword = txtCurrentPassword.Password;
+            }
+        }
+
+        private void txtNewPassword_PasswordChanged(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (this.DataContext is SettingsViewModel viewModel)
+            {
+                viewModel.NewPassword = txtNewPassword.Password;
+            }
+        }
+
+        private void txtConfirmPassword_PasswordChanged(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (this.DataContext is SettingsViewModel viewModel)
+            {
+                viewModel.ConfirmPassword = txtConfirmPassword.Password;
+            }
         }
     }
 }
