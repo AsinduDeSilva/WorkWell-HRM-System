8a2d301bbed07f00ca72aa22ed8f73bcc19fee15
Asindu De Silva <asindudesilva@gmail.com>
2025-05-02 13:37:03 +0530

    Bug Fixed

--- WorkWell/MainWindow.xaml.cs
+++ WorkWell/MainWindow.xaml.cs
@@ -9,9 +9,8 @@ public partial class MainWindow : Window
 {
     public MainWindow()
     {
-        //EmailService.SendEmail("asindudesilva2@gmail.com", "Test", "<h1>Hello!</h1><p>This is a test email sent from C# code.</p>");
         InitializeComponent();
         FrameManagerService.MainFrame = MainFrame;
-        MainFrame.Navigate(new AdminView());
+        MainFrame.Navigate(new Signin());
     }
 }
