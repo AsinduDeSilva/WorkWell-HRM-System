33e18eb12ee326015aefccd6e9d5a69cec5f5164
DewmiS <dewmisamadhipathiranage@gmail.com>
2025-05-02 13:41:56 +0530

    Updated EmployeePayrollPage

--- WorkWell/Views/HR/EmployeePayrollPage.xaml.cs
+++ WorkWell/Views/HR/EmployeePayrollPage.xaml.cs
@@ -1,28 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows.Controls;
+using WorkWell.ViewModels;
+using WorkWell.Models;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
 namespace WorkWell.Views.HR
 {
-    /// <summary>
-    /// Interaction logic for EmployeePage.xaml
-    /// </summary>
+    /// <summary>  
+    /// Interaction logic for EmployeePage.xaml  
+    /// </summary>  
     public partial class EmployeePayrollPage : Page
     {
-        public EmployeePayrollPage()
+        public EmployeePayrollPage(int employeeId, string name)
         {
             InitializeComponent();
+            DataContext = new EmployeePayrollPageViewModel(employeeId, name);
         }
     }
 }
