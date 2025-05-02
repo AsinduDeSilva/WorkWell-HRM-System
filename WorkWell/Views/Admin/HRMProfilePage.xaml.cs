f5415eb10a679f29ceef87316f4cab24efe9742e
Asindu De Silva <asindudesilva@gmail.com>
2025-05-02 17:20:07 +0530

    Created HRM Profile View

---
+++ WorkWell/Views/Admin/HRMProfilePage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+using WorkWell.ViewModels;
+
+namespace WorkWell.Views.Admin
+{
+    /// <summary>
+    /// Interaction logic for HRMProfilePage.xaml
+    /// </summary>
+    public partial class HRMProfilePage : Page
+    {
+        public HRMProfilePage(int hrManagerId)
+        {
+            InitializeComponent();
+            DataContext = new HRMProfilePageViewModel(hrManagerId);  
+        }
+    }
+}
